ecab9635ed93fa78e0d84a312fe96cfd4d0a6ddd
soniamitchell <soniamitchell@users.noreply.github.com>
2021-01-12 19:25:54 +0000

    initial commit

---
+++ inst/dummy_package/package0301-package.R
@@ -0,0 +1,20 @@
+#' package0301
+#'
+#' \code{package0301} is a dummy package containing a simple birth-death
+#' difference equation model.
+#'
+#' @author
+#' Sonia Mitchell <sonia.mitchell@glasgow.ac.uk> (maintainer) \cr
+#' Richard Reeve <richard.reeve@glasgow.ac.uk>
+
+#' @name package0301-package
+#' @aliases package0301
+#' @docType package
+#'
+#' @examples
+#' \dontrun{
+#' # To run the demo
+#' demo("0305-run-birth-death.R", package = "package0301")
+#' }
+#'
+NULL
