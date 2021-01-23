feb5d8ba55fbab21fc501596bd1b479371b4f197
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-01-23 23:28:28 +0000

    add new function

---
+++ R/practical_series_one.R
@@ -0,0 +1,19 @@
+#' practical_series_one
+#'
+#' This function will populate the current working directory with scripts and
+#' functions from practical series 1.
+#'
+#' @export
+#'
+practical_series_one <- function() {
+
+  # Populate project_name
+  files <- system.file("dummy_project", package = "RPiR")
+  files <- dir(files, full.names = TRUE)
+
+  for (i in seq_len(length(files))) {
+    file.copy(files[i], ".")
+    usethis::ui_done(paste("Writing", usethis::ui_value(basename(files[i]))))
+  }
+
+}
