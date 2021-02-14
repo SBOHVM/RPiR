5193eaaff61cd5d63ddf0b830447cda8a7487ba9
Richard Reeve <git@richardreeve.net>
2021-02-14 22:09:19 +0000

    Add in project lecture

--- R/export_materials.R
+++ R/export_materials.R
@@ -7,10 +7,11 @@ export_materials <- function(materials, type = "exercises") {
   if (type == "exercises") {
     c(materials[grepl("A.?-", materials)],
       materials[grepl("1.?-[12345]", materials)],
-      materials[grepl("[234].?-", materials)])
+      materials[grepl("[234].?-", materials)],
+      materials[grepl("project", materials)])
   } else if (type == "lectures") {
     unique(c(materials[grepl("[123456789].?-", materials)],
-             materials[grepl("1[012].?-", materials)]))
+             materials[grepl("1[0123].?-", materials)]))
   } else {
     stop(paste("Unknown material type:", type))
   }
