7ce0e483f5c380159d4254b99345f3c561506bdf
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-10-26 12:45:55 +0100

    tidy up

--- R/plot_simple.R
+++ R/plot_simple.R
@@ -9,12 +9,15 @@
 #'
 #' @param populations Data frame with columns corresponding to different
 #' population segments and a 'time' column
-#' @param new.graph (optionally) whether to start a new graph, default TRUE
-#' @param xlim (optionally, for new graphs) the limits of the x axis,
+#' @param new.graph (optional) whether to start a new graph, default TRUE
+#' @param xlim (optional, for new graphs) the limits of the x axis,
 #' default min to max time
-#' @param ylim (optionally, for new graphs) the limits of the y axis,
+#' @param ylim (optional, for new graphs) the limits of the y axis,
 #' default min to max pop size
-#' @param lty (optionally) the line type for all lines on the graph, default 1
+#' @param lty (optional) the line type for all lines on the graph, default 1
+#' @param legend (optional) legend position; choose from "topleft", "top",
+#' "topright" (default), "left", "center", "right", "bottomleft", "bottom", or
+#' "bottomright"
 #'
 #' @export
 #'
@@ -23,15 +26,13 @@
 #'                  die = exp(seq(10, 0, by = -0.1)))
 #' plot_simple(df, lty = c(2, 3))
 #'
-plot_simple <- function(populations, new.graph=TRUE, xlim=NA, ylim=NA,
-                        lty=1) {
+plot_simple <- function(populations, new.graph = TRUE, xlim, ylim,
+                        lty = 1, legend = "topright") {
   # First make sure there's a time column in the data frame being plotted
   if (any(colnames(populations) == "time")) {
     time <- populations$time
     populations$time <- NULL
-    # If need be, set x-limits on plot from it
-    if (is.na(xlim[1]))
-      xlim <- c(min(time), max(time))
+
   } else # Otherwise complain and stop...
     stop("No time info available - data frame must have a column called 'time'")
 
@@ -40,8 +41,12 @@ plot_simple <- function(populations, new.graph=TRUE, xlim=NA, ylim=NA,
   # Create our own standard set of colours
   line.cols <- seq_along(labels)
 
+  # Get x-limits on graph from input if we haven't set our own
+  if (missing(xlim))
+    xlim <- c(min(time), max(time))
+
   # Get y-limits on graph from input if we haven't set our own
-  if (is.na(ylim[1]))
+  if (missing(ylim))
     ylim <- c(0, max(rowSums(populations)))
 
   # And plot the individual columns against time
@@ -54,7 +59,8 @@ plot_simple <- function(populations, new.graph=TRUE, xlim=NA, ylim=NA,
            xlim = xlim, ylim = ylim,
            xlab = "time", ylab = "population size",
            type = "l", col = line.cols[index], lty = lty[index])
-      graphics::legend("topright", legend = labels, lty = lty, col = line.cols)
+      graphics::legend(legend, legend = labels,
+                       lty = lty, col = line.cols, box.lty = 0, inset = .02)
       new.graph <- FALSE
     } else # Or as a new line on an existing graph
       graphics::lines(time, this.pop, col = line.cols[index], lty = lty[index])
