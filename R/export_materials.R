419411fd8d838812a741fd90bc7e97acd31cdd15
Richard Reeve <git@richardreeve.net>
2021-11-18 23:18:48 +0000

    Export some more exercises

--- R/export_materials.R
+++ R/export_materials.R
@@ -5,14 +5,13 @@
 #'
 export_materials <- function(materials, type = "exercises") {
   if (type == "exercises") {
-    c(materials[grepl("0.?-", materials)],
-      materials[grepl("1.?-", materials)]#,
-#      materials[grepl("[234].?-", materials)],
+    c(materials[grepl("[012].?-", materials)]#,
+#      materials[grepl("[34].?-", materials)],
 #      materials[grepl("project", materials)]
       )
   } else if (type == "lectures") {
-    unique(c(materials[grepl("1.?-", materials)]#,
-#             materials[grepl("[23456789].?-", materials)],
+    unique(c(materials[grepl("[12].?-", materials)]#,
+#             materials[grepl("[3456789].?-", materials)],
 #             materials[grepl("1[0123].?-", materials)]
              )
            )
