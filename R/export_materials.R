8a3d9bb4f3680795a9c2624924c29b5d8989c03a
Richard Reeve <git@richardreeve.net>
2021-11-18 15:31:18 +0000

    Get rid of courses beyond where we are.

--- R/export_materials.R
+++ R/export_materials.R
@@ -5,13 +5,17 @@
 #'
 export_materials <- function(materials, type = "exercises") {
   if (type == "exercises") {
-    c(materials[grepl("A.?-", materials)],
-      materials[grepl("1.?-[12345]", materials)],
-      materials[grepl("[234].?-", materials)],
-      materials[grepl("project", materials)])
+    c(materials[grepl("0.?-", materials)],
+      materials[grepl("1.?-[12345]", materials)]#,
+#      materials[grepl("[234].?-", materials)],
+#      materials[grepl("project", materials)]
+      )
   } else if (type == "lectures") {
-    unique(c(materials[grepl("[123456789].?-", materials)],
-             materials[grepl("1[0123].?-", materials)]))
+    unique(c(materials[grepl("1.?-", materials)]#,
+#             materials[grepl("[23456789].?-", materials)],
+#             materials[grepl("1[0123].?-", materials)]
+             )
+           )
   } else {
     stop(paste("Unknown material type:", type))
   }
