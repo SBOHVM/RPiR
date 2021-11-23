131dc15282035caf708b9968c34419274a20ec04
Richard Reeve <git@richardreeve.net>
2021-11-23 23:56:56 +0000

    Adding in lecture 3

--- R/export_materials.R
+++ R/export_materials.R
@@ -10,8 +10,8 @@ export_materials <- function(materials, type = "exercises") {
 #      materials[grepl("project", materials)]
       )
   } else if (type == "lectures") {
-    unique(c(materials[grepl("RPiR[12][a-z]?-", materials)]#,
-#             materials[grepl("RPiR[3456789][a-z]?-", materials)],
+    unique(c(materials[grepl("RPiR[123][a-z]?-", materials)]#,
+#             materials[grepl("RPiR[456789][a-z]?-", materials)],
 #             materials[grepl("RPiR1[0123][a-z]?-", materials)]
              )
            )
