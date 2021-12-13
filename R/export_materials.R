d68bd7f3ee7dd3125188c87baafd4acc274f0ffc
Richard Reeve <git@richardreeve.net>
2021-12-13 10:24:06 +0000

    Export talks and practicals

--- R/export_materials.R
+++ R/export_materials.R
@@ -6,16 +6,15 @@
 #' @keywords internal
 export_materials <- function(materials, type = "exercises") {
   if (type == "exercises") {
-    c(materials[grepl("practical[A123][a-z]?-", materials)]#,
-#      materials[grepl("practical4[a-z]?-", materials)],
+    c(materials[grepl("practical[A1234][a-z]?-", materials)]#,
 #      materials[grepl("project", materials)]
       )
   } else if (type == "lectures") {
-    unique(c(materials[grepl("RPiR[12345678][a-z]?-", materials)]#,
-#             materials[grepl("RPiR9[a-z]?-", materials)],
-#             materials[grepl("RPiR1[0123][a-z]?-", materials)]
-             )
-           )
+    unique(c(materials[grepl("RPiR[123456789][a-z]?-", materials)],
+             materials[grepl("RPiR1[01][a-z]?-", materials)]
+#             materials[grepl("RPiR1[23][a-z]?-", materials)]
+    )
+    )
   } else {
     stop(paste("Unknown material type:", type))
   }
